281ddefd28f110d4287e9ca6d7b30f7d63b8cdc9
Matebook <adilbek200620@gmail.com>
2025-04-24 11:30:21 +0500

    at first is empty methods without unctionality

---
+++ src/MyBST.java
@@ -0,0 +1,19 @@
+
+public class MyBST {
+    private Node root;
+    private class Node {
+        private K key;
+        private V val;
+        private Node left;
+        private Node right;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+        public void put(K key, V val) {}
+        public V get(K key) {}
+        public void delete(K key) {}
+        public Iterable<K> iterator() {}
+    }
+}
