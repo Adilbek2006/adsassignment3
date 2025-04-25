66dd71511a4971dfbb76373a2db92914e26c2460
Matebook <adilbek200620@gmail.com>
2025-04-26 00:55:01 +0500

    put method implementation

--- src/MyBST.java
+++ src/MyBST.java
@@ -1,6 +1,6 @@
-
-public class MyBST {
+public class MyBST<K extends Comparable<K>, V> {
     private Node root;
+    private int size;
     private class Node {
         private K key;
         private V val;
@@ -11,9 +11,42 @@ public class MyBST {
             this.key = key;
             this.val = val;
         }
-        public void put(K key, V val) {}
-        public V get(K key) {}
-        public void delete(K key) {}
-        public Iterable<K> iterator() {}
     }
+    public class Entry {
+        private K key;
+        private V value;
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        public K getKey() {
+            return key;
+        }
+        public V getValue () {
+            return value;
+        }
+    }
+    public void put(K key, V val) {
+        root = put(root,K key, V val);
+    }
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            size++;
+            return new Node(key, val);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left,key, val);
+        }
+        else if (cmp > 0) {
+            node.right = put(node.right, key, val);
+        }
+        else {
+            node.val = val;
+        }
+        return node;
+    }
+    public V get(K key) {}
+    public void delete(K key) {}
+    public Iterable<K> iterator() {}
 }
