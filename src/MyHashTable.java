fca23781a12949caf459260fe4834936ecd0c62f
Matebook <adilbek200620@gmail.com>
2025-04-28 18:40:17 +0500

    full my hash table is done

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,5 +1,7 @@
-public class MyHashTable {
-    private class HashNode {
+import java.util.LinkedList;
+
+public class MyHashTable<K, V> {
+    private class HashNode<K, V> {
         private K key;
         private V value;
         private HashNode<K, V> next;
@@ -14,16 +16,84 @@ public class MyHashTable {
             return"{" + key + " " + value + "}";
         }
     }
-    private HashNode<K, V>[] chainArray;
+    private LinkedList<HashNode<K, V>>[] chainArray;
     private int M = 11;
     private int size;
 
-    public MyHashTable() {}
-    public MyHashTable(int M) {}
-    private int hash(K key) {}
-    public void put(K key, V value) {}
-    public V get(K key) {}
-    public V remove(K key) {}
-    public boolean contains(V value) {}
-    public K getKey(V value) {}
+    public MyHashTable() {
+        chainArray = new LinkedList[M];
+        for (int i = 0; i < M; i++) {
+            chainArray[i] = new LinkedList<>();
+        }
+    }
+    public MyHashTable(int M) {
+        this.M = M;
+        chainArray = new LinkedList[M];
+        for (int i = 0; i < M; i++) {
+            chainArray[i] = new LinkedList<>();
+        }
+    }
+    private int hash(K key) {
+        return Math.abs(key.hashCode()) % M;
+    }
+    public void put(K key, V value) {
+        int index = hash(key);
+        for(HashNode<K, V> node : chainArray[index]) {
+            if(node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+        }
+        chainArray[index].add(new HashNode<>(key, value));
+        size++;
+    }
+    public V get(K key) {
+        int index = hash(key);
+        for(HashNode<K, V> node : chainArray[index]) {
+            if(node.key.equals(key)) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+    public V remove(K key) {
+        int index = hash(key);
+        for(HashNode<K, V> node : chainArray[index]) {
+            if(node.key.equals(key)) {
+                V value = node.value;
+                chainArray[index].remove(node);
+                size--;
+                return value;
+            }
+        }
+        return null;
+    }
+    public boolean contains(V value) {
+        for(LinkedList<HashNode<K,V>> bucket : chainArray) {
+            for(HashNode<K, V> node : bucket) {
+                if(node.value.equals(value)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public K getKey(V value) {
+        for(LinkedList<HashNode<K, V>> bucket : chainArray) {
+            for(HashNode<K, V> node : bucket) {
+                if(node.value.equals(value)) {
+                    return node.key;
+                }
+            }
+        }
+        return null;
+    }
+    public int getSize() {
+        return size;
+    }
+    public void printBucketSize() {
+        for(int i = 0;i < M; i++) {
+            System.out.println("bucket " + i + ": " + chainArray[i].size() + " elements");
+        }
+    }
 }
